f5a3d20e9f459b8139c64b4411bf20208f80f5cd
stephane-vanraes <stephane.vanraes@gmail.com>
2024-01-22 21:08:46 +0100

    start of order details

---
+++ src/lib/components/OrderDetails.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { cart } from '$lib/stores/cart';
+</script>
+
+<div>
+	{#each Object.values($cart) as pizza}
+		<label for={pizza.id}>{pizza.name}</label>
+		<input type="number" bind:value={$cart[pizza.id].amount} />
+		<button on:click={() => cart.remove(pizza)}>Remove</button>
+	{/each}
+</div>
+
+<style>
+	div {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+</style>
--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,15 +1,8 @@
 <script lang="ts">
+	import OrderDetails from '$lib/components/OrderDetails.svelte';
 	import { cart } from '$lib/stores/cart';
 </script>
 
 <h1>Your Order</h1>
 
-<ul>
-	{#each Object.values($cart) as order}
-		<li>
-			<span>{order.name}</span>
-			<span>{order.amount}</span>
-			<span>{order.amount * order.price} NOK</span>
-		</li>
-	{/each}
-</ul>
+<OrderDetails />
