f443ea45913f14ba1eed2aab9a45b4f13730fa90
stephane-vanraes <stephane.vanraes@gmail.com>
2024-01-30 07:36:05 +0100

    add loader for slow orders

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,29 @@
+<div>
+  <div></div>
+</div>
+
+<style>
+  div {
+    display: grid;
+    place-items: center;
+    height: 20vh;
+    width: 100%;
+  }
+
+  div > div {
+    background-image: linear-gradient(64deg, 
+      var(--primary-color) 30%, 
+      var(--secondary-color) 30%,
+      var(--secondary-color) 70%, 
+      var(--primary-color) 70%);
+    background-size: 2rem 2rem;
+    height: 1rem;
+    border-radius: 1rem;
+    animation: slide 1000ms linear infinite;
+  }
+
+  @keyframes slide {
+    0% { background-position: 0rem; }
+    100% { background-position: 2rem;}
+  }
+</style>
--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,20 +1,36 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import { applyAction, enhance, } from '$app/forms';
+	import Loader from '$lib/components/Loader.svelte';
 	import OrderDetails from '$lib/components/OrderDetails.svelte';
 	import { cart } from '$lib/stores/cart.js';
+	import type { ActionResult } from '@sveltejs/kit';
 
   export let form
 
+  let loading = false
+
   $: form && cart.clear();
+
+
+  function handleOrder() {
+    loading = true
+    return async ({ result } : { result: ActionResult }) => {
+      loading = false
+      cart.clear();
+      await applyAction(result)
+    }
+  }
 </script>
 
 <h1>Your Order</h1>
 
-{#if form}
+{#if loading}
+  <Loader />
+{:else if form}
   <p>Thank you for your order</p>
   <p>{form?.order_id}</p>
 {:else}
-  <form method="POST" use:enhance>
+  <form method="POST" use:enhance={handleOrder}>
   <OrderDetails />
   <button type="submit">I want Pizza</button>
   <button type="button" on:click={cart.demo}>Fill Cart</button>
